b1b64048884d087e038dba410fee9d3233c5de7b
Dan Gebhardt <dan@cerebris.com>
2012-02-27 15:23:31 -0500

    added tests for resourceUrl() and duplicateProperties()

--- tests/tests/ember-resource.js
+++ tests/tests/ember-resource.js
@@ -11,7 +11,7 @@ module("Ember.Resource", {
 });
 
 test("should serialize its properties to JSON", function() {
-  var props = {"id": 1, "first_name": "Joe", "last_name": "Blow"};
+  var props = {id: 1, first_name: "Joe", last_name: "Blow"};
 
   var contact = Contact.create(props);
   var serialized  = contact.serialize();
@@ -21,7 +21,7 @@ test("should serialize its properties to JSON", function() {
 });
 
 test("should deserialize its properties to JSON", function() {
-  var props = {"id": 1, "first_name": "Joe", "last_name": "Blow"};
+  var props = {id: 1, first_name: "Joe", last_name: "Blow"};
 
   var contact = Contact.create();
   contact.deserialize(props);
@@ -29,6 +29,20 @@ test("should deserialize its properties to JSON", function() {
   equal(contact.get("id"),         props.id,         "id matches");
   equal(contact.get("first_name"), props.first_name, "first_name matches");
   equal(contact.get("last_name"),  props.last_name,  "last_name matches");
+});
+
+test("should duplicate properties from another resource", function() {
+  var contact = Contact.create({id: 1, first_name: "Joe", last_name: "Blow"});
+  var contact2 = Contact.create();
+  contact2.duplicateProperties(contact);
+
+  equal(contact2.get("first_name"), contact.get("first_name"), "first_name matches");
+  equal(contact2.get("last_name"),  contact.get("last_name"),  "last_name matches");
+});
+
+test("should determine a resourceUrl based upon id", function() {
+  var contact = Contact.create({id: 1});
+  equal(contact._resourceUrl(), "/contacts/1");
 });
 
 test("should find a resource via ajax", function() {
